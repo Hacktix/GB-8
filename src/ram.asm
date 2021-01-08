95ee64f827435f9de1382d98c1ae7add965b2271
Hacktix <zecmad16@htlkaindorf.at>
2021-01-08 14:28:37 +0100

    Exported RAM variable labels

--- src/ram.asm
+++ src/ram.asm
@@ -1,16 +1,17 @@
 SECTION "WRAM", WRAM0
-wBaseMemory: ds $1000
+wBaseMemory:: ds $1000
 
 _start_sysvars:
 
 ; Register Pointers
-wRegV:       ds 16
-wRegI:       dw
-wRegDelay:   db
-wRegSound:   db
+wRegV::       ds 16
+wRegI::       dw
+wRegDelay::   db
+wRegSound::   db
 
 ; Stack
-wStack: ds 32
-wStackEnd:
+wSP::         db
+wStack::      ds 32
+wStackEnd::
 
 _end_sysvars:
