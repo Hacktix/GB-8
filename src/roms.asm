9bd8a1f6c6373770949ecc1ac2a68fca6643851b
Hacktix <zecmad16@htlkaindorf.at>
2021-01-13 14:58:00 +0100

    Fixed incorrect button mapping order

--- src/roms.asm
+++ src/roms.asm
@@ -15,7 +15,7 @@ SECTION "ROM Files", ROM0
 ;  unassigned button.
 ; 
 ;  Button Mapping bytes in order:
-;  A, B, Select, Start, DPad Right, DPad Left, DPad Up, DPad Down
+;  Start, Select, B, A, DPad Down, DPad Up, DPad Left, DPad Right
 ; ------------------------------------------------------------------------------
 
 ; ------------------------------------------------------------------------------
@@ -26,7 +26,7 @@ Pong::
 db "Pong (1P)", 0
 
 ; Button Mapping
-db $FF, $FF, $FF, $FF, $FF, $FF, $01, $04
+db $FF, $FF, $FF, $FF, $01, $04, $FF, $FF
 
 ; ROM Size
 dw pongDataEnd - pongData
